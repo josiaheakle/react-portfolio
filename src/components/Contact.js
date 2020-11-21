63ccc612156a0e31970e6467c8c15b189b6d0ad7
Josiah Eakle <josiaheakle@gmail.com>
2020-11-21 13:32:33 -0500

    adding toast now

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,14 +2,32 @@
 import "../css/Contact.css"
 
 import {motion} from "framer-motion"
+import Clipboard from "clipboard"
+import { useEffect } from "react"
 
 const Contact = ( props ) => {
+
+    const initClipboard = () => {
+        const clipboard = new Clipboard('.copy-to-clipboard-button');
+
+        clipboard.on('success', function() {
+            console.log(`copied`)
+
+        });
+        clipboard.on('error', function() {
+            console.log(`not copied`)
+        });
+    }
+
+    useEffect(() => {
+        initClipboard()
+    }, [])
 
     return(
         <motion.div exit={ {opacity: 0} } initial={ {opacity: 0} } animate={ {opacity:1} } className='Contact main-container'>
             <h2> Contact Me </h2> 
             <span> If you are interested in working with me, email me at <span className='to-color'> dev@josiaheakle.com </span>. </span> 
-            <button  className='copy-button'> Copy Email </button> 
+            <button data-clipboard-text="dev@josiaheakle.com" className='copy-to-clipboard-button'> Copy Email </button> 
         </motion.div>
     );
 }
